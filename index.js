a1a27a0ffba2a2b3a799fa44a2cf404f2928d174
Luca Campana <luca.campa.94@gmail.com>
2019-04-12 16:57:56 +0200

    Added axios

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.post('/api/payment', (req, res) => {
+    res.sendStatus(201);
+})
+
 app.use(express.static('public'));
 
 app.listen(3000, () => console.log('Started on 3000'));
